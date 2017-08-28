105ece0bdb790fb775ab4fbd129f698c1dc3fea2
Rafael Vargas <rafael.vargas@dotz.com.br>
2017-08-28 16:54:09 -0300

    removing dist

--- dist/out-tsc/core/entities/example.entity.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ExampleEntity = (function () {
-    function ExampleEntity() {
-    }
-    return ExampleEntity;
-}());
-exports.ExampleEntity = ExampleEntity;
-//# sourceMappingURL=example.entity.js.map
--- dist/out-tsc/infrastructure/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Env = {
-    bigchainAppId: process.env['app_id'],
-    bigchainAppKey: process.env['app_key'],
-};
-//# sourceMappingURL=env.js.map
